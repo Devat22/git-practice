0823acfc560431a7a4a9235c00dad3cd730f7692
Devat22 <dev.ac.da90@gmail.com>
2019-08-17 12:49:27 -0500

    upload the project

---
+++ app/src/main/java/com/rjt/kotlinlec/Demo.kt
@@ -0,0 +1,47 @@
+package com.rjt.kotlinlec
+
+import java.util.*
+
+fun main() {
+//    var a = "Hello World"
+//    var b = "This is my first kotlin fun"
+//    val c = b //instantiating is ok
+//
+//    print(c)
+//    print("Enter your name")
+//    val name:String? = readLine()
+//    println(name)
+//    print("Enter your age")
+//    val read= Scanner(System.`in`)
+//    val age:Int = read.nextInt()
+//    println("Your age is : "+age)
+//    val a:Int = 1
+//    val b:Int = 3
+//    val returnValue:Int = if(a>b){
+//         a
+//    }else{
+//         b
+//    }
+//    println(returnValue)
+    var number = 8
+    var numberProvided:String = when(number){
+        1->"one"
+        2->"two"
+        3->"three"
+        4->"four"
+        5->"five"
+        8->"eight"
+        9->"nine"
+        else -> "invalid number"
+    }
+    println("You provided $numberProvided")
+
+}
+//2 types of strings -Escaped:"you are" you can't use it with multiple lines
+// -Raw:""you"" you can write multiple lines
+
+//for not specified data type we use Unit as return for functions
+//  Nothing(not known by now),Any for variable data type
+//readline() // read line of input it is only for string
+//Scanner for reading any type of data
+//Is is used here as if in java
